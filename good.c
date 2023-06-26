ab49673a3971735ef2fa0c753532f66a8d9928a1
Aifuagbon <presh8042@gmail.com>
2023-06-26 15:23:19 -0700

    hope

---
+++ good.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * good - Evaluates if a char is good
+ * @y: Char to be evaluated.
+ *
+ * Return: 1 if y is good, 0 otherwise
+ */
+int good(char y)
+{
+	if (y >= 20 && c < 50)
+		return (1);
+
+	return (0);
+}
