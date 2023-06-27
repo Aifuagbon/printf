c8016ccb598d673c1602bfd665859d853a01d83b
Emmanuel Ndua <emmanuel.nduan@gmail.com>
2023-06-26 21:15:52 -0700

    aniceli1.c

--- aniceli1.c
+++ aniceli1.c
@@ -15,25 +15,25 @@ int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 
 {
-	int i = BUFF_SIZE - 2;
+	int c = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-		buffer[i--] = '0';
+		buffer[c--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
+		buffer[c--] = (num % 10) + '0';
 		num /= 10;
 	}
 
-	i++;
+	c++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, c, buffer, flags, width, precision, size));
 }
 
 /**
@@ -51,7 +51,7 @@ int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 
 {
-	int i = BUFF_SIZE - 2;
+	int c = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 
 	unsigned long int init_num = num;
@@ -61,22 +61,22 @@ int print_octal(va_list types, char buffer[],
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-		buffer[i--] = '0';
+		buffer[c--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
 	{
-		buffer[i--] = (num % 8) + '0';
+		buffer[c--] = (num % 8) + '0';
 		num /= 8;
 	}
 
 	if (flags & F_HASH && init_num != 0)
-		buffer[i--] = '0';
+		buffer[c--] = '0';
 
-	i++;
+	c++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, c, buffer, flags, width, precision, size));
 }
 
 /**
