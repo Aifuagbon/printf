563644a83d9ace12dc188dec941efdb1d0eee5c3
Emmanuel Ndua <emmanuel.nduan@gmail.com>
2023-06-27 07:13:10 -0700

    get_size.c

--- get_precision.c
+++ get_precision.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: Lists of arguments to be printed
- * @list: list of arguments
+ * get_precision - Calculates precision for printing
+ * @format: Formatted string in which to print arguments
+ * @i: Lists arguments to be printed
+ * @list: list arguments
  *
  * Return: Precision
  */
